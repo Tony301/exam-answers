52f49172d2c95b1d6cc27cd789aeccf6576caa69
Tony301 <60770795+Tony301@users.noreply.github.com>
2020-02-11 14:27:14 +0530

    Add files via upload
    exam answers for java workshop

---
+++ pryamid.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class pryamid {
+	   public static void main(String[] args) {
+           int rows = 5, k = 0;
+           for(int i = 1; i <= rows; ++i, k = 0) {
+               for(int n = 1; n <= rows - i; ++n) {
+                   System.out.print("  ");
+               }
+               while(k != 2 * i - 1) {
+                   System.out.print("* ");
+                   ++k;
+               }
+               System.out.println();
+           }
+       }
+}
